f28bf6b0c6e5ce9878c25904d9c3aeaf909a91d4
Turis Nyasar <turis.nyasar@gmail.com>
2023-10-05 13:02:18 +0700

    modified:   database/sekolah.sql

--- database/sekolah.sql
+++ database/sekolah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2023 at 07:06 AM
+-- Generation Time: Oct 05, 2023 at 08:02 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -66,7 +66,11 @@ INSERT INTO `foto_kegiatan` (`ID`, `ID_kegiatan`, `foto`, `caption`) VALUES
 (2, 1, 'B_1696310089.jpg', '2'),
 (3, 2, 'A_1696310171.jpg', ''),
 (4, 3, 'A_1696310216.jpg', '1'),
-(5, 4, 'A_1696310284.jpg', '');
+(5, 4, 'A_1696310284.jpg', ''),
+(6, 7, 'A_1696479981.jpg', 'adaasdas'),
+(7, 7, 'B_1696479981.jpg', 'asdasdsa'),
+(8, 7, 'C_1696479981.jpg', 'asdasda'),
+(9, 7, 'D_1696479981.jpg', 'asdasdas');
 
 -- --------------------------------------------------------
 
@@ -92,7 +96,8 @@ INSERT INTO `kegiatan` (`ID_kegiatan`, `judul_kegiatan`, `artikel_kegiatan`, `tg
 (3, 'kungfu terbang sedang berkelahi', 'kungfu terbang sedang berkelahi', '2023-10-03 07:16:56', 1),
 (4, 'ustadz kucing sedang tausiah', 'ustadz kucing sedang tausiah', '2023-10-03 07:18:04', 1),
 (5, 'Berita belum ada isi', 'Entah apa isinya saya pun tidak tahu', '2023-10-04 06:34:09', 1),
-(6, 'Belum ada Judul', 'Ini bukan berita. Ini lagu Iwan Fals.', '2023-10-04 06:34:30', 1);
+(6, 'Belum ada Judul', 'Ini bukan berita. Ini lagu Iwan Fals.', '2023-10-04 06:34:30', 1),
+(7, 'Jadwal Setoran Hapalan', 'Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document. To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries. Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme. Save time in Word with new buttons that show up where you need them.\r\nTo change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign. Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off - even on another device. Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document. To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar.\r\nClick Insert and then choose the elements you want from the different galleries. Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme. Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign. Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off - even on another device.\r\nVideo provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document. To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries. Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme. Save time in Word with new buttons that show up where you need them.\r\n&nbsp;', '2023-10-05 06:26:21', 1);
 
 --
 -- Indexes for dumped tables
@@ -133,13 +138,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `foto_kegiatan`
 --
 ALTER TABLE `foto_kegiatan`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  MODIFY `ID_kegiatan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID_kegiatan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
